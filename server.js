e1517bf567ed642a30f74af0c32ac7ecd4fa8567
MuBaRaK1325 <abubakarmubarak3456@gmail.com>
2026-04-25 03:09:54 +0100

    fffff

--- server.js
+++ server.js
@@ -1058,5 +1058,5 @@ app.get("/", (req, res) => {
   res.send("MAYCONNECT API Live");
 });
 /* ================= START ================= */
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
