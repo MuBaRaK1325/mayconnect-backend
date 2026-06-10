1dcc32522f5e9830f080ffdfe8ae01b8ca0ac0dd
MuBaRaK1325 <abubakarmubarak3456@gmail.com>
2026-06-10 01:05:18 +0100

    bbbb

--- server.js
+++ server.js
@@ -172,7 +172,7 @@ const MAITAMA_NETWORK_MAP = {
   'airtel': 2,
   'glo': 3,
   '9mobile': 4,
-  'etisalat': 4
+  
 };
 
 function getMaitamaNetworkId(networkName) {
@@ -1952,14 +1952,6 @@ app.post("/api/buy-data", auth, buyDataLimiter, async (req, res) => {
 
 /* ================= BUY AIRTIME - Maitama: 1=MTN, 2=AIRTEL, 3=GLO, 4=9MOBILE ================= */
 // MAITAMA NETWORK MAP
-const MAITAMA_NETWORK_MAP = {
-  'mtn': 1,
-  'airtel': 2,
-  'glo': 3,
-  '9mobile': 4,
-  'etisalat': 4
-};
-
 function getMaitamaNetworkId(networkName) {
   return MAITAMA_NETWORK_MAP[String(networkName).toLowerCase()] || null;
 }
